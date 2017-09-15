7d22d0713b1bc479d95c6c85ee59c63d50c69455
Behnam Ghabaei <behnamghabaei@104-1-54-35.lightspeed.irvnca.sbcglobal.net>
2017-09-14 23:21:03 -0700

    References, Definitions, and Constructors added.

--- WellsFargo/oldBank.py
+++ WellsFargo/oldBank.py
@@ -1,3 +1,20 @@
-# This is Behnam.
+    # Wells Fargo - Automatic Teller Machine (References)
 
-print ("Hello World!");
+bankMoto = str ("Together Well Go Far");
+bankWelcome = str ("Welcome to Wells Fargo. " + bankMoto);
+bankGoodbye = str ("Goodbye from Wells Fargo. " + bankMoto);
+askBankNumber = str ("Enter Bank Number:" );
+askBankKey = str ("Enter Bank Key:");
+bankNumber = int (input(askBankNumber));
+bankKey = int (input(askBankKey));
+
+    # Wells Fargo - Automatic Teller Machine (Definitions)
+
+def goBank():
+print (bankWelcome);
+print (askBankNumber);
+print (askBankKey);
+print (bankGoodbye);
+
+    # Wells Fargo - Automatic Teller Machine (Constructors)
+
